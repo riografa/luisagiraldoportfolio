eea83084244a6238719778fe5b55290ef1840e5f
Keishmer Pérez <145237779+Keishmer00@users.noreply.github.com>
2025-10-10 11:39:17 -0500

    act git actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://riografa.github.io',
+  base: 'riografa.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
+
+
